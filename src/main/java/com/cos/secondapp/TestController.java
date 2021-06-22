b09b4dea729f9bfceeedf061c7a7e6ad91bf2fe6
skacjswo1234 <https://github.com/skacjswo1234>
2021-06-22 16:02:34 +0900

    2021-06-22

--- src/main/java/com/cos/secondapp/TestController.java
+++ src/main/java/com/cos/secondapp/TestController.java
@@ -1,6 +1,9 @@
 package com.cos.secondapp;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 주소요청을 하는것
@@ -8,15 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController 
 public class TestController {
 
-	// http://localhost:8000/test1
-	@GetMapping("/test1")
-	public String test1() {
-		return"test1 ok";
+	// http://localhost:8000/
+	@GetMapping("/")
+	public String get() {
+		return"get";
 	}
 	
-	// http://localhost:8000/test2
-	@GetMapping("/test2")
-	public String test2() {
-		return"test2 ok";
+	// http://localhost:8000/
+	@PostMapping("/")
+		public String post(String name, String password) {
+		System.out.println(name);
+		System.out.println(password);
+		return"post";
+	}
+	// http://localhost:8000/
+	@PutMapping("/")
+	public String put(String data) {
+			System.out.println(data);
+		return"put";
+	}
+	// http://localhost:8000/
+	@DeleteMapping("/")
+	public String delete() {
+		return"delete";
 	}
 }
