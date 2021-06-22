d6731e260c09a991a9f7314e7583c4632bdf2bdb
skacjswo1234 <https://github.com/skacjswo1234>
2021-06-22 17:55:34 +0900

    2021-06-22

--- src/main/java/com/cos/secondapp/TestController.java
+++ src/main/java/com/cos/secondapp/TestController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 주소요청을 하는것
@@ -19,15 +20,16 @@ public class TestController {
 	
 	// http://localhost:8000/
 	@PostMapping("/")
-		public String post(String name, String password) {
-		System.out.println(name);
-		System.out.println(password);
-		return"post";
+		public User post( User data) {
+		System.out.println(data.getUsername());
+		System.out.println(data.getPassword());
+		return data;
 	}
 	// http://localhost:8000/
 	@PutMapping("/")
-	public String put(String data) {
-			System.out.println(data);
+	public String put(@RequestBody User data) { // x-www-form-urlencoded 기본전략 (오브젝트로 받으면 자동 파싱)
+			System.out.println(data.getUsername()); //RequestBody => BufferedReader ( 오브젝트로 받으면 자동 파싱)
+			System.out.println(data.getPassword());
 		return"put";
 	}
 	// http://localhost:8000/
